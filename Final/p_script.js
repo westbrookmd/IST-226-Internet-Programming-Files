8417ae48d01f1323826be2775307da95b3b62d28
westbrookmd <71563811+westbrookmd@users.noreply.github.com>
2021-12-03 12:56:00 -0500

    Styling and Javascript Button Function
    Created a JS function that creates event handlers for each button within a class. Created an array constant that stores all of the names of the button classes. Buttons collapse by using display element

---
+++ Final/p_script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/*
+
+   Author: Marshall Westbrook
+   Date:  11/19/2021
+
+   Filename:   p_script.js  
+
+   Function List:
+   collapsibleHandler - A way to create click events on buttons with class names
+   
+	
+*/
+
+
+
+const listOfButtonClasses = ["chapter0", "chapter1", "chapter2", "chapter3",
+ "chapter4", "chapter5", "chapter6", "chapter7", "chapter8", "chapter9", "chatper10"];
+var i;
+for(i = 0; i < listOfButtonClasses.length; i++)
+{
+    collapsibleHandler(document.getElementsByClassName(listOfButtonClasses[i]));
+}
+
+function collapsibleHandler(coll){
+    var i;
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var content = this.nextElementSibling;
+          if (content.style.display === "flex") {
+            content.style.display = "none";
+          } else {
+            content.style.display = "flex";
+          }
+        });
+      }
+}
