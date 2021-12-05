f43c952f1a120c24351dbdbe11ca7d9cd9fe1319
westbrookmd <westbrookmd@email.wofford.edu>
2021-12-04 23:15:05 -0500

    Final Portfolio Submission
    Portfolio completed with extra elements in each rubric category

--- Final/p_script.js
+++ Final/p_script.js
@@ -3,7 +3,7 @@
 /*
 
    Author: Marshall Westbrook
-   Date:  11/19/2021
+   Date:  12/4/2021
 
    Filename:   p_script.js  
 
@@ -16,7 +16,7 @@
 
 
 const listOfButtonClasses = ["chapter0", "chapter1", "chapter2", "chapter3",
- "chapter4", "chapter5", "chapter6", "chapter7", "chapter8", "chapter9", "chatper10"];
+ "chapter4", "chapter5", "chapter6", "chapter7", "chapter8", "chapter9", "chapter10"];
 var i;
 for(i = 0; i < listOfButtonClasses.length; i++)
 {
